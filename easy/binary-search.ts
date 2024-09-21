6df88cd1cb94b434f0c251260b6fc67679992cdf
lucas <lucas.chorolqui@gmail.com>
2024-09-20 21:58:34 -0300

    binary search algorithm

---
+++ easy/binary-search.ts
@@ -0,0 +1,19 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+function searchInsert(nums: number[], target: number): number {
+  let left: number = 0;
+  let right: number = nums.length - 1;
+  while (left <= right) {
+    const mid: number = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+}
